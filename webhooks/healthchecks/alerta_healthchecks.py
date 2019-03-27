ed65cfc2c57b735ec7f450e3ee5ba1f7167fc17c
Grégory BITTAN <gregory@Gregorys-MacBook-Pro.local>
2019-03-27 10:24:21 +0100

    Use query string before payload.

--- webhooks/healthchecks/alerta_healthchecks.py
+++ webhooks/healthchecks/alerta_healthchecks.py
@@ -13,20 +13,22 @@ class HealthchecksWebhook(WebhookBase):
         group ='Cron'
         text = ''
         tags = []
-        status = 'Down'
+        status = 'DOWN'
+        event = 'health'
+
         #tags=['{}'.format(v) for k, v in payload['event']['tags']],
         #attributes={'tags': ['{}'.format(k, v) in query_string.items() if k.startswith("TAG")]},
         return Alert(
             service=['healthchecks'],
-            resource=payload['name'],
-            text=payload.get('text', text),
-            event=payload['event'],
-            group=payload.get('group', group),
-            severity=payload.get('severity', severity),
-            value=payload.get('status', status),
-            environment=payload.get('environment', environment),
+            resource=query_string.get('name',payload.get('name', environment)),
+            text=query_string.get('text',payload.get('text', text)),
+            event=query_string.get('event',payload.get('event', event)),
+            group=query_string.get('group',payload.get('group', group)),
+            severity=query_string.get('severity',payload.get('severity', severity)),
+            value=query_string.get('status',payload.get('status', status)),
+            environment=query_string.get('environment',payload.get('environment', environment)),
             tags=[],
             attributes={},
-            origin='Healthchecks/{}'.format(payload['code']),
+            origin='Healthchecks/{}'.format(query_string.get('code',payload.get('code', 'Unknown'))),
             raw_data=json.dumps(payload, indent=4)
         )
